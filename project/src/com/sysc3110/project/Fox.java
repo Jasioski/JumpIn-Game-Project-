81f9838c3bcbdab08d4f8e445cf2af794e9d2de3
Rafi Khan <rafi@rafikhan.io>
2019-10-16 18:25:43 -0400

    added Fox

---
+++ project/src/com/sysc3110/project/Fox.java
@@ -0,0 +1,41 @@
+package com.sysc3110.project;
+
+public class Fox extends BoardItem {
+
+	private int tailRow;
+	private int tailColumn;
+
+	private static void validateArguments(int headRow, int headColumn, int tailRow, int tailColumn)
+			throws IllegalArgumentException {
+
+		if (headColumn == tailColumn && headRow == tailRow) {
+			throw new IllegalArgumentException("The fox cannot have its tail and head in the same position");
+		}
+
+		if (Math.abs(headColumn - tailColumn) > 1 || Math.abs(headRow - tailRow) > 1) {
+			throw new IllegalArgumentException(
+					"The fox cannot have its tail more than a unit " + "a way from its head");
+		}
+
+		if (Math.abs(headColumn - tailColumn) == Math.abs(headRow - tailRow)) {
+			throw new IllegalArgumentException("The fox cannot have its tail diagonal to its head");
+		}
+	}
+
+	public Fox(int headRow, int headColumn, int tailRow, int tailColumn) {
+		super(headRow, headColumn);
+
+		validateArguments(headRow, headColumn, tailRow, tailColumn);
+
+		this.tailRow = tailRow;
+		this.tailColumn = tailColumn;
+	}
+
+	public int getTailRow() {
+		return tailRow;
+	}
+
+	public int getTailColumn() {
+		return tailColumn;
+	}
+}
