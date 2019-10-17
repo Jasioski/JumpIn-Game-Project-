fbe721c97789ceb4842402f8812fc4997eee9a9c
Rafi Khan <rafi@rafikhan.io>
2019-10-16 23:29:21 -0400

    added rabbit

---
+++ project/src/main/java/project/Rabbit.java
@@ -0,0 +1,8 @@
+package project;
+
+public class Rabbit extends BoardItem {
+
+	public Rabbit(int row, int column) {
+		super(row, column);
+	}
+}
---
+++ project/src/test/java/project/RabbitTest.java
@@ -0,0 +1,20 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class RabbitTest {
+
+	@Test
+	void testConstructor() {
+		int row = 0;
+		int column = 1;
+		
+		Rabbit rabbit = new Rabbit(row, column);
+		
+		assertEquals(rabbit.getRow(), row, "row should be same");
+		assertEquals(rabbit.getColumn(), column, "column should be same");
+	}
+
+}
