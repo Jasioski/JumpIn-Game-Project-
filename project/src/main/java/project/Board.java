fb3b72ab4a9591bba952c775c897ccd74c16eb72
Rafi Khan <rafi@rafikhan.io>
2019-10-18 17:56:35 -0400

    removed unused imports

--- project/src/main/java/project/Board.java
+++ project/src/main/java/project/Board.java
@@ -1,8 +1,7 @@
 package project;
 
-import java.util.ArrayList;
+import java.util.ArrayList;	
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Board {
