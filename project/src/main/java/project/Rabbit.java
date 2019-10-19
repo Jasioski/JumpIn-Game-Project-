0e5955b75409456d1e193018c21b9939357ee99e
kamransagheir <kamran.sagheir@yahoo.com>
2019-10-19 02:30:23 -0400

    Worked on Rabbit Jumping Impl

--- project/src/main/java/project/Rabbit.java
+++ project/src/main/java/project/Rabbit.java
@@ -1,8 +1,11 @@
 package project;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class Rabbit extends BoardItem {
+public class Rabbit extends BoardItem implements Jumpable {
 
 	private static final Character RABBIT_DISPLAY_CHARACTER = 'R';
 	
@@ -38,6 +41,70 @@ public class Rabbit extends BoardItem {
 	 */
 	public Coordinate getCoordinate() {
 		return this.getCoordinates().get(0);
+	}
+
+	@Override
+	public List<Coordinate> jump(Direction direction, List<BoardItem> slice) throws JumpObstacleException {
+		// TODO Auto-generated method stub
+		List<Coordinate> newCoordinates = new ArrayList<>();
+
+		List<Coordinate> oldCoordinates = this.getCoordinates();
+		switch(direction) {
+		case RIGHT:
+			try {
+				newCoordinates = this.jumpRight(slice);
+			} catch (JumpObstacleException e) {
+				this.setCoordinates(oldCoordinates);
+				throw e;
+			}
+			break;
+		default: 
+			break;
+		
+		}
+		return newCoordinates;
+	}
+
+	private List<Coordinate> jumpRight(List<BoardItem> slice) throws JumpObstacleException {
+		Coordinate currentCoordinate = this.getCoordinate();
+		Coordinate newCoordinate = new Coordinate(currentCoordinate.row, currentCoordinate.column + 1);
+
+		// Get all coordinates in the slice without duplicates
+		Set<Coordinate> sliceCoordinates = new HashSet<Coordinate>();
+
+		for (BoardItem item : slice) {
+			sliceCoordinates.addAll(item.getCoordinates());
+		}
+
+		// loop over all the items in the slice
+		boolean hitObstacle = slice.stream().anyMatch(sliceItem -> {
+			// check if is at the same coordinate as one of the new coordinates
+			if (sliceItem.getCoordinates().contains(newCoordinate)) {
+				// match if the item is not empty
+				// and not the current item
+				if ((sliceItem.getClass() != EmptyBoardItem.class)) {
+					if (!(sliceItem.equals(this))) {
+						return true;
+					}
+				}
+			}
+			// do not match if the item is empty or the current item
+			return false;
+		});
+
+		if (hitObstacle) {
+			this.setCoordinate(newCoordinate);
+			System.out.println("Hit rabbit...");
+			return jumpRight(slice);
+		}
+		else {
+			List<Coordinate> newCoordinates = new ArrayList<>();
+			newCoordinates.add(newCoordinate);
+			this.setCoordinate(newCoordinate);
+			System.out.println("Encountered empty spot.." + newCoordinate);
+			return newCoordinates;			
+		}
+
 	}
 
 }
--- project/src/test/java/project/RabbitTest.java
+++ project/src/test/java/project/RabbitTest.java
@@ -2,6 +2,7 @@ package project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -95,5 +96,48 @@ class RabbitTest {
 			item.setCoordinates(finalCoordinates);
 		});
 	}
+	
+	@Test 
+	/**
+	 * Jump right over one rabbit 
+	 * 
+	 */
+	 void testJumpRightOne() {
+		
+		Rabbit rabbitJumping = new Rabbit(0, 0);
+		Rabbit rabbitObstacle = new Rabbit(0, 1);
+		
+		// Setup slice
+		// index:  0 1 2 3
+		// Layout: RJ RO E E
+		List<BoardItem> slice = new ArrayList<BoardItem>();
+		slice.add(rabbitJumping);
+		slice.add(rabbitObstacle);
+		slice.add(new EmptyBoardItem(0, 0));
+		slice.add(new EmptyBoardItem(0, 3));
+		
+		// Jump Rabbit
+		Direction moveDirection = Direction.RIGHT;
+		
+		try {
+			List<Coordinate> newCoordinates = rabbitJumping.jump(moveDirection, slice);
+			// Expected result
+			// index:  0 1 2 3
+			// Layout: E R0 RJ E
+			
+			Coordinate newRabbitJumpingCoordinate = new Coordinate(0, 2);
+			assertEquals(newRabbitJumpingCoordinate ,rabbitJumping.getCoordinate(), "the rabbit should be at"
+					+ "the new location" );
+			
+			assertEquals(newRabbitJumpingCoordinate, newCoordinates.get(0), "The method should return new coordinate for"
+					+ "the rabbit");
+			
+		} catch (Exception e) {
+			fail("Exception was thrown");
+		}
+		
+		
+
+	} 
 
 }
