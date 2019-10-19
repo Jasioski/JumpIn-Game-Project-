22440307c7d8d4ee15e121f8dcad58f3aa1561e9
Rafi Khan <rafi@missioncontrolspaceservices.com>
2019-10-19 17:49:46 -0400

    added exception

--- project/src/main/java/project/JumpObstacleException.java
+++ project/src/main/java/project/JumpObstacleException.java
@@ -1,5 +1,6 @@
 package project;
 
+// TODO: Deprecate because it is no longer being used
 public class JumpObstacleException extends Exception {
 	public JumpObstacleException(String s) {
 		super(s);
