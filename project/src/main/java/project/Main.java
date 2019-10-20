bf6b99b11018eda31b72d4491badacf1f6a10fd0
kamransagheir <kamran.sagheir@yahoo.com>
2019-10-19 21:14:38 -0400

    Bug Fix

--- project/src/main/java/project/Main.java
+++ project/src/main/java/project/Main.java
@@ -63,73 +63,90 @@ public class Main {
 
 		do {
 			try {
+
+				System.out.println("\n" + board.toString());
+				System.out.println("Please type one of the following commands");
+				System.out.println(
+						"1) Jump Rabbit row(e.g., 1),colums e.g., 2 (current coordinates) Direction(Right, Left, Up, Down)");
+				System.out.println("2) Slide Fox row(e.g., 1), colums e.g., 2 Number of boad uints/ spaces (e.g., 2)");
+				System.out.println("Sample command: \n Jump Rabbit 1,2 Right");
+				System.out.println("Sample command: \n Slide Fox 0,2 2 Left");
+
+				System.out.println("Please enter command: ");
+				String userInput = scanner.nextLine();
+				System.out.println("dsfhdfsuhf:  " + userInput);
+				String[] commands = userInput.toString().split(" ");
 				
-					System.out.println("\n" + board.toString());
-					System.out.println("Please type one of the following commands");
-					System.out.println(
-							"1) Jump Rabbit row(e.g., 1),colums e.g., 2 (current coordinates) Direction(Right, Left, Up, Down)");
-					System.out.println(
-							"2) Slide Fox row(e.g., 1), colums e.g., 2 Number of boad uints/ spaces (e.g., 2)");
-					System.out.println("Sample command: \n Jump Rabbit 1,2 Right");
-					System.out.println("Sample command: \n Slide Fox 0,2 2 Left");
+				
+				int unitsToMove = -1;
+				String userEnteredDirection = "";
+				String[] rowColumn;
+				int row = -1;
+				int column = -1;
+				String userEnteredCoordinates = "";
+				String itemType = "";
+				String moveType = "";
+				if (!commands[0].equals("Jump") || !commands[0].equals("Slide")) {
+					System.out.println("Please enter the correct move type e..g, either Jump or Slide");
+				}
+				
+				if(!commands[0].equals("")) {
+				moveType = commands[0];
+				
+				if (!commands[0].equals("Rabbit") || !commands[0].equals("Fox")) {
+					System.out.println("Please enter the item type e..g, either Rabbit or Fox");
+				}
+				
+				if(!commands[1].toString().equals("")) {
+				itemType = commands[1];
+				}
+				
+				if(!commands[2].equals("")) {
+				userEnteredCoordinates = commands[2];
+				}
 
-					System.out.println("Please enter command: ");
-					if (scanner.nextLine() != null) {
-					String userInput = scanner.nextLine();
-					System.out.println("dsfhdfsuhf:  " + userInput);
-					String[] commands = userInput.toString().split(" ");
-					if (!commands[0].equals("Jump") || !commands[0].equals("Slide")) {
-						System.out.println("Please enter the correct move type e..g, either Jump or Slide");
-					}
-					String moveType = commands[0];
-					if (!commands[0].equals("Rabbit") || !commands[0].equals("Fox")) {
-						System.out.println("Please enter the item type e..g, either Rabbit or Fox");
-					}
-					String itemType = commands[1];
+				// Checking if the last part of the command is an Integer or a string
+				try {
+					// if integer, assign it to unitsToMove else; will get default value -1
+					unitsToMove = Integer.parseInt(commands[3]);
 
-					String userEnteredCoordinates = commands[2];
-					int unitsToMove = -1;
-					String userEnteredDirection = "";
-					String[] rowColumn;
-					int row = -1;
-					int column = -1;
-					// Checking if the last part of the command is an Integer or a string
-					try {
-						// if integer, assign it to unitsToMove else; will get default value -1
-						unitsToMove = Integer.parseInt(commands[3]);
+				} catch (Exception e) {
 
-					} catch (Exception e) {
+				}
+				// if String assign it to direction
+				if(!commands[4].equals("")) {
+				userEnteredDirection = commands[4];
+				}
+				
 
-					}
-					// if String assign it to direction
-					userEnteredDirection = commands[4];
+				System.out.println(
+						"moveType: " + moveType + " itemType: " + itemType + " coordinates: " + userEnteredCoordinates
+								+ " unitsToMove: " + unitsToMove + " direction: " + userEnteredDirection);
+				rowColumn = userEnteredCoordinates.split(",", 2);
+				row = Integer.parseInt(rowColumn[0]);
+				
+				column = Integer.parseInt(rowColumn[1]);
+				}
+				if (row == -1 || column == -1) {
+					System.out.println("Please enter correct coordinates in format e.g., row,column i.e., 2,3");
+				}
+				coordinates = new Coordinate(row, column);
+				direction = StringToEnum(userEnteredDirection);
+				if (moveType.equals("Jump")) {
+					// the input deals with the Rabbits
 
-					System.out.println("moveType: " + moveType + " itemType: " + itemType + " coordinates: "
-							+ userEnteredCoordinates + " unitsToMove: " + unitsToMove + " direction: "
-							+ userEnteredDirection);
-					rowColumn = userEnteredCoordinates.split(",", 2);
-					row = Integer.parseInt(rowColumn[0]);
-					column = Integer.parseInt(rowColumn[1]);
-					if (row == -1 || column == -1) {
-						System.out.println("Please enter correct coordinates in format e.g., row,column i.e., 2,3");
-					}
-					coordinates = new Coordinate(row, column);
-					direction = StringToEnum(userEnteredDirection);
-					if (moveType.equals("Jump")) {
-						// the input deals with the Rabbits
+					board.jump(direction, coordinates);
 
-						board.jump(direction, coordinates);
+				} else if (moveType.equals("Slide")) {
+					// deals with the Foxes
+					if (unitsToMove != -1) {
+						System.out.println("here....");
 
-					} else if (moveType.equals("Slide")) {
-						// deals with the Foxes
-						if (unitsToMove != -1) {
-							System.out.println("here....");
+						board.slide(direction, unitsToMove, coordinates);
 
-							board.slide(direction, unitsToMove, coordinates);
-
-						}
 					}
 				}
+
 			} catch (JumpObstacleException e) {
 				// TODO GET RID OF IT
 				e.printStackTrace();
@@ -158,6 +175,7 @@ public class Main {
 								+ " Please enteer the command with different coordinates.");
 
 			}
+			
 		} while (board.currentGameState == GameState.IN_PROGRESS);
 		scanner.close();
 		System.out.println(board.getCurrentGameState());
