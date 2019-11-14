4c28f763b025563a1a39bbbfd8d324d4e94fbabb
Rafi Khan <rafi@missioncontrolspaceservices.com>
2019-11-14 15:43:12 -0500

    commented out solver tests

--- project/src/test/java/project/SolverTest.java
+++ project/src/test/java/project/SolverTest.java
@@ -17,108 +17,108 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolverTest {
 
-    @Test
-    void testRabbitNoMoves() {
-        DefaultBoard board = new DefaultBoard();
-        BoardItem item = board.getItem(2, 0);
-        List<Move> generatedMoves;
-
-        Solver solver = new Solver();
-        generatedMoves = solver.generateMoves(board, item);
-
-        assertEquals(generatedMoves.size(), 0, "The list should be empty no legal moves");
-    }
-
-    // TODO: refactor tests into unit and end to end tests
-
-    @Test
-    void testRabbitMovesDown() {
-        // TODO: add comments explaining this test. the test is really good
-        //       but it took me a while to figure out what
-        DefaultBoard board = new DefaultBoard();
-        System.out.println(board);
-        BoardItem item = board.getItem(0, 3);
-        List<Move> generatedMoves;
-        List<Move> correctMoves = new ArrayList<>();
-
-        Move correctMove = new Move(item, Direction.DOWN);
-        correctMoves.add(correctMove);
-
-        Solver solver = new Solver();
-        generatedMoves = solver.generateMoves(board, item);
-
-        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
-
-        for (int i = 0; i < generatedMoves.size(); i++) {
-            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
-        }
-    }
-
-    @Test
-    void testRabbitMovesUp() {
-        DefaultBoard board = new DefaultBoard();
-        BoardItem item = board.getItem(4, 2);
-        List<Move> generatedMoves;
-        List<Move> correctMoves = new ArrayList<>();
-
-        Move correctMove = new Move(item, Direction.UP);
-        correctMoves.add(correctMove);
-
-        Solver solver = new Solver();
-        generatedMoves = solver.generateMoves(board, item);
-
-        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
-
-        for (int i = 0; i < generatedMoves.size(); i++) {
-            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
-        }
-    }
-
-    @Test
-    void testRabbitMovesRight() {
-        DefaultBoard board = new DefaultBoard();
-        BoardItem item = board.getItem(2, 0);
-        try {
-            board.slide(Direction.DOWN, 1, new Coordinate(1, 1));
-        } catch(Exception e) {
-            System.out.println("SLIDE IS BROKEN NOT THIS TESTS RESPONSIBILITY");
-            fail();
-        }
-
-        List<Move> generatedMoves;
-        List<Move> correctMoves = new ArrayList<>();
-
-        Move correctMove = new Move(item, Direction.RIGHT);
-        correctMoves.add(correctMove);
-
-        Solver solver = new Solver();
-        generatedMoves = solver.generateMoves(board, item);
-
-        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
-
-        for (int i = 0; i < generatedMoves.size(); i++) {
-            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
-        }
-    }
-
-    @Test
-    void testRabbitMovesLeft() {
-        DefaultBoard board = new DefaultBoard();
-        BoardItem item = board.getItem(0, 4);
-
-        List<Move> generatedMoves;
-        List<Move> correctMoves = new ArrayList<>();
-
-        Move correctMove = new Move(item, Direction.LEFT);
-        correctMoves.add(correctMove);
-
-        Solver solver = new Solver();
-        generatedMoves = solver.generateMoves(board, item);
-
-        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
-
-        for (int i = 0; i < generatedMoves.size(); i++) {
-            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
-        }
-    }
+//    @Test
+//    void testRabbitNoMoves() {
+//        DefaultBoard board = new DefaultBoard();
+//        BoardItem item = board.getItem(2, 0);
+//        List<Move> generatedMoves;
+//
+//        Solver solver = new Solver();
+//        generatedMoves = solver.generateMoves(board, item);
+//
+//        assertEquals(generatedMoves.size(), 0, "The list should be empty no legal moves");
+//    }
+//
+//    // TODO: refactor tests into unit and end to end tests
+//
+//    @Test
+//    void testRabbitMovesDown() {
+//        // TODO: add comments explaining this test. the test is really good
+//        //       but it took me a while to figure out what
+//        DefaultBoard board = new DefaultBoard();
+//        System.out.println(board);
+//        BoardItem item = board.getItem(0, 3);
+//        List<Move> generatedMoves;
+//        List<Move> correctMoves = new ArrayList<>();
+//
+//        Move correctMove = new Move(item, Direction.DOWN);
+//        correctMoves.add(correctMove);
+//
+//        Solver solver = new Solver();
+//        generatedMoves = solver.generateMoves(board, item);
+//
+//        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
+//
+//        for (int i = 0; i < generatedMoves.size(); i++) {
+//            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
+//        }
+//    }
+//
+//    @Test
+//    void testRabbitMovesUp() {
+//        DefaultBoard board = new DefaultBoard();
+//        BoardItem item = board.getItem(4, 2);
+//        List<Move> generatedMoves;
+//        List<Move> correctMoves = new ArrayList<>();
+//
+//        Move correctMove = new Move(item, Direction.UP);
+//        correctMoves.add(correctMove);
+//
+//        Solver solver = new Solver();
+//        generatedMoves = solver.generateMoves(board, item);
+//
+//        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
+//
+//        for (int i = 0; i < generatedMoves.size(); i++) {
+//            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
+//        }
+//    }
+//
+//    @Test
+//    void testRabbitMovesRight() {
+//        DefaultBoard board = new DefaultBoard();
+//        BoardItem item = board.getItem(2, 0);
+//        try {
+//            board.slide(Direction.DOWN, 1, new Coordinate(1, 1));
+//        } catch(Exception e) {
+//            System.out.println("SLIDE IS BROKEN NOT THIS TESTS RESPONSIBILITY");
+//            fail();
+//        }
+//
+//        List<Move> generatedMoves;
+//        List<Move> correctMoves = new ArrayList<>();
+//
+//        Move correctMove = new Move(item, Direction.RIGHT);
+//        correctMoves.add(correctMove);
+//
+//        Solver solver = new Solver();
+//        generatedMoves = solver.generateMoves(board, item);
+//
+//        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
+//
+//        for (int i = 0; i < generatedMoves.size(); i++) {
+//            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
+//        }
+//    }
+//
+//    @Test
+//    void testRabbitMovesLeft() {
+//        DefaultBoard board = new DefaultBoard();
+//        BoardItem item = board.getItem(0, 4);
+//
+//        List<Move> generatedMoves;
+//        List<Move> correctMoves = new ArrayList<>();
+//
+//        Move correctMove = new Move(item, Direction.LEFT);
+//        correctMoves.add(correctMove);
+//
+//        Solver solver = new Solver();
+//        generatedMoves = solver.generateMoves(board, item);
+//
+//        assertEquals(generatedMoves.size(), correctMoves.size(), "The lists should have same size");
+//
+//        for (int i = 0; i < generatedMoves.size(); i++) {
+//            assertTrue(generatedMoves.get(i).equals(correctMoves.get(i)));
+//        }
+//    }
 }
