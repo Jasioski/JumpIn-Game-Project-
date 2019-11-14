1246412cccfb8d4838dd26ee9af9e57b2e22239f
Jasioski <grabkowski.john@gmail.com>
2019-11-14 14:27:53 -0500

    Created new solver and main classes

---
+++ project/src/main/java/project/solverRefactored/Move.java
@@ -0,0 +1,56 @@
+package project.solverRefactored;
+
+import project.modelRefactored.BoardItem;
+import project.modelRefactored.Coordinate;
+//import project.modelRefactored.Direction;
+
+public class Move {
+
+    //todo: make direction enum class
+    public BoardItem item;
+    public Coordinate initial;
+    public Coordinate ending;
+
+    /*
+    public Direction direction;
+
+    public Move(BoardItem item, Direction direction) {
+        this(item, direction, null, null); //todo: refactor the null (smelly code)
+    }
+
+    public Move(BoardItem item, Direction direction, Coordinate initial, Coordinate ending) { //for slideable need end coords
+        this.item = item;
+        this.direction = direction;
+        this.initial = initial;
+        this.ending = ending;
+    }
+
+    */
+
+    public boolean equals(Move move) {
+        if (!(this.item == move.item)) {
+            return false;
+        }
+        if (!(this.initial == move.initial)) {
+            return false;
+        }
+        /*
+        if (!(this.direction == move.direction)) {
+            return false;
+        }
+         */
+        if (!(this.ending == move.ending)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String toString()  {
+        String str = "";
+        str += "Item: " + item.getClass() + " from: " + initial + " to: " + ending;
+
+        return str;
+    }
+
+}
---
+++ project/src/main/java/project/solverRefactored/Solver.java
@@ -0,0 +1,23 @@
+package project.solverRefactored;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import project.modelRefactored.Board;
+import project.modelRefactored.BoardItem;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solver {
+    Board board;
+
+    //Use logger
+    public static Logger logger = LogManager.getLogger(Solver.class);
+
+    public Solver() {
+
+    }
+
+
+
+}
