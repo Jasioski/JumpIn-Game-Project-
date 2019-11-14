40b8a6076ee0d81070af0067b9e768e9824cffd6
Rafi Khan <rafi@missioncontrolspaceservices.com>
2019-11-14 15:43:19 -0500

    Changed key of the board hashmap to typeof Coordinate
    This was done because the 2d representation only needs to be aware of
    the coordinate of the position, and not whether the item of that
    coordinate has more than a single coordinate.

--- project/src/main/java/project/modelRefactored/Board.java
+++ project/src/main/java/project/modelRefactored/Board.java
@@ -1,14 +1,13 @@
 package project.modelRefactored;
 
 import io.atlassian.fugue.Either;
+import io.atlassian.fugue.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.pcollections.HashTreePMap;
 import org.pcollections.PMap;
 
 import java.util.List;
-
-
 
 public class Board {
 
@@ -17,7 +16,7 @@ public class Board {
     private static Logger logger = LogManager.getLogger(Board.class);
 
     //todo: override and throw exception on map.get
-    private PMap<Either<Coordinate, List<Coordinate>>, BoardItem> items;
+    private PMap<Coordinate, BoardItem> items;
 
     public Board(int rows, int columns) {
         items = HashTreePMap.empty();
@@ -29,7 +28,7 @@ public class Board {
             for (int column = 0; column < numberOfColumns; column++) {
                 Coordinate currentCoordinate = new Coordinate(row, column);
                 BoardItem itemToAdd = new EmptyBoardItem(currentCoordinate);
-                items = items.plus(Either.left(currentCoordinate), itemToAdd);
+                items = items.plus(currentCoordinate, itemToAdd);
             }
         }
     }
@@ -42,27 +41,39 @@ public class Board {
         this.items = board.items;
     }
 
+	/**
+	 * Set an item on a board while preserving purity
+	 * @param board reference to the board state you want to apply the
+	 *                 transformation to
+	 * @param item the item you want to add to the board
+	 * @return a board which is a result of the applied transformation
+	 */
 	public static Board setItem(Board board, BoardItem item) {
         Board thisBoard = new Board(board);
 
-        thisBoard.items = thisBoard.items.plus(item.coordinate, item);
+        if (item.coordinate.isLeft()) {
+			logger.trace("found left item");
+			Coordinate coordinate = item.coordinate.left().get();
+			thisBoard.items = thisBoard.items.plus(coordinate, item);
+		}
 
-        return thisBoard;
+        if (item.coordinate.isRight()) {
+        	logger.trace("found right type");
+        	Pair<Coordinate,Coordinate> coordinate =
+					item.coordinate.right().get();
+
+        	thisBoard.items = thisBoard.items.plus(coordinate.left(), item);
+			thisBoard.items = thisBoard.items.plus(coordinate.right(), item);
+		}
+
+		return thisBoard;
     }
 
-    private BoardItem getItem(Either<Coordinate, List<Coordinate>> coordinate) {
+    public BoardItem getItem(Coordinate coordinate) {
         return this.items.get(coordinate);
     }
 
-	private BoardItem getItem(Coordinate coordinate) {
-		return getItem(Either.left(coordinate));
-	}
-
-	private BoardItem getItem(List<Coordinate> coordinate) {
-		return getItem(Either.right(coordinate));
-	}
-
-	public PMap<Either<Coordinate, List<Coordinate>>, BoardItem> getItems() {
+	public PMap<Coordinate, BoardItem> getItems() {
         return items;
     }
 
--- project/src/main/java/project/modelRefactored/BoardItem.java
+++ project/src/main/java/project/modelRefactored/BoardItem.java
@@ -1,12 +1,14 @@
 package project.modelRefactored;
 
 import io.atlassian.fugue.Either;
+import io.atlassian.fugue.Pair;
+
 import java.util.List;
 
 public abstract class BoardItem {
-    public final Either<Coordinate, List<Coordinate>> coordinate;
+    public final Either<Coordinate, Pair<Coordinate, Coordinate>> coordinate;
 
-    public BoardItem(List<Coordinate> coordinate) {
+    public BoardItem(Pair<Coordinate, Coordinate> coordinate) {
         this.coordinate = Either.right(coordinate);
     }
 
