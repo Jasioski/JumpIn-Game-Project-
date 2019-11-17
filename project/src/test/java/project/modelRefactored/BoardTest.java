5bed8b63d276f11655f72edbd85c3aeb7e16eeb2
Jasioski <grabkowski.john@gmail.com>
2019-11-16 22:18:00 -0500

    Board test jumping holes

--- project/src/test/java/project/modelRefactored/BoardTest.java
+++ project/src/test/java/project/modelRefactored/BoardTest.java
@@ -1,5 +1,7 @@
 package project.modelRefactored;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import project.model.Direction;
 
@@ -8,6 +10,8 @@ import com.google.common.base.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
+
+    private static Logger logger = LogManager.getLogger(Board.class);
 
     @Test
     void testJumpRightOne() {
@@ -78,10 +82,9 @@ public class BoardTest {
 
         Board newBoard;
         try {
-            //TODO: Debug prints. Remove after finished jump out of hole method
-            System.out.println(board.getItem(initialCoordinate).getClass());
+            logger.debug(board.getItem(initialCoordinate).getClass());
             Hole testHole = (Hole) board.getItem(initialCoordinate);
-            System.out.println(testHole.containingItem.get().getClass());
+            logger.debug(testHole.containingItem.get().getClass());
 
             newBoard = board.jump(Direction.RIGHT, initialCoordinate);
 
@@ -112,8 +115,111 @@ public class BoardTest {
                     EmptyBoardItem, "item at 0, 0 should be empty");
 
         } catch (Exception e) {
-            //TODO: Debug prints. Remove after finished jump out of hole method
-            System.out.println(e.getMessage());
+            logger.debug(e.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    void testJumpIntoHole() {
+        // Slice setup
+        // R M H
+
+        //Expected
+        //E M HR
+
+        Coordinate initialCoordinate = new Coordinate(0, 0);
+        Rabbit initialRabbit = new Rabbit(initialCoordinate);
+        Coordinate expectedJumpCoordinate = new Coordinate(0, 2);
+
+        Board board = new Board(1,3);
+        board = board.setItem(new Mushroom(new Coordinate(0 ,1)));
+
+        Hole destinationHole = new Hole(expectedJumpCoordinate,
+                Optional.absent());
+
+        board = board.setItem(initialRabbit);
+        board = board.setItem(destinationHole);
+
+        Board newBoard;
+        try {
+            newBoard = board.jump(Direction.RIGHT, initialCoordinate);
+
+            Hole newHole = (Hole) newBoard.getItem(expectedJumpCoordinate);
+
+            //new board should be updated
+            assertTrue(newHole instanceof Hole, "0, 2 should be a " +
+                    "hole");
+            assertTrue(newHole.containingItem.get() instanceof Rabbit,
+                    "0, 2 " +
+                            "should have a Rabbit");
+            assertTrue(newBoard.getItem(initialCoordinate) instanceof
+                    EmptyBoardItem, "0, 0 should now be empty");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    void testJumpOutOfHoleIntoAnother() {
+        // Slice setup
+        // HR M H
+
+        //Expected
+        //H M HR
+
+        Coordinate initialCoordinate = new Coordinate(0, 0);
+        Rabbit initialRabbit = new Rabbit(initialCoordinate);
+        Coordinate expectedJumpCoordinate = new Coordinate(0, 2);
+
+        Board board = new Board(1,3);
+        board = board.setItem(new Mushroom(new Coordinate(0 ,1)));
+
+        Hole originalHole = new Hole(initialCoordinate,
+                Optional.of(initialRabbit));
+        Hole destinationHole = new Hole(expectedJumpCoordinate,
+                Optional.absent());
+
+        board = board.setItem(originalHole);
+        board = board.setItem(destinationHole);
+
+        Board newBoard;
+        try {
+            logger.debug(board.getItem(initialCoordinate).getClass());
+            Hole testHole = (Hole) board.getItem(initialCoordinate);
+            logger.debug(testHole.containingItem.get().getClass());
+
+            newBoard = board.jump(Direction.RIGHT, initialCoordinate);
+
+            Hole newHole = (Hole) newBoard.getItem(expectedJumpCoordinate);
+
+            //new board should be updated
+            assertTrue(newHole instanceof Hole, "0, 2 should be a " +
+                    "hole");
+            assertTrue(newHole.containingItem.get() instanceof Rabbit,
+                    "0, 2 " +
+                    "should have a Rabbit");
+
+            Hole oldHole = (Hole) newBoard.getItem(initialCoordinate);
+            //old hole should still be there
+            assertTrue(oldHole instanceof Hole, "0, 0 should be a " +
+                    "hole");
+            assertTrue(!oldHole.containingItem.isPresent(), "0, 0 " +
+                    "should be empty Hole");
+
+            //old board should remain same
+            oldHole = (Hole) board.getItem(initialCoordinate);
+            assertTrue(oldHole.containingItem.get() instanceof Rabbit,
+                    "0, 0 should be Hole containing Rabbit");
+
+            newHole = (Hole) board.getItem(expectedJumpCoordinate);
+            assertTrue(!newHole.containingItem.isPresent(),
+                    "0, 2 should be Hole containing Rabbit");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
             fail();
         }
     }
