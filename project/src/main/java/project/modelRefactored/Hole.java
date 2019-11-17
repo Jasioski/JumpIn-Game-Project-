cd7359b8ca0198a0f5b520519ffeb0d1293205a8
AntonAroche <anton.aroche@gmail.com>
2019-11-16 19:26:10 -0500

    Added javadoc

--- project/src/main/java/project/modelRefactored/Hole.java
+++ project/src/main/java/project/modelRefactored/Hole.java
@@ -6,16 +6,29 @@ import io.atlassian.fugue.Pair;
 import org.pcollections.PMap;
 import project.model.Direction;
 
+/**
+ * Represents a hole object on the board.
+ */
 public final class Hole extends SingleBoardItem implements Elevated {
-
+    /**
+     * The item that the hole may contain.
+     */
     public final Optional<Containable> containingItem;
 
+    /**
+     * Constructs a new hole with a coordinate and optional item.
+     * @param coordinate The coordinate where the hole is located.
+     * @param containingItem The optional item that it can contain.
+     */
     public Hole(Coordinate coordinate, Optional<Containable> containingItem) {
         super(coordinate);
         this.containingItem = containingItem;
     }
 
-
+    /**
+     * Returns true if the hole acts as an obstacle.
+     * @return True if the hole contains an item.
+     */
     @Override
     public boolean isObstacle() {
         if (containingItem.isPresent()) {
@@ -26,6 +39,13 @@ public final class Hole extends SingleBoardItem implements Elevated {
         return false;
     }
 
+    /**
+     * Attempts to jump a rabbit out of a hole.
+     * @param direction The direction that the rabbit must jump.
+     * @param slice The slice where the jump occurs.
+     * @return A pair containing the new empty hole, and either the rabbit or the new hole it is found in.
+     * @throws InvalidMoveException If the hole is empty or the rabbit cannot jump.
+     */
     public Pair<Hole, Either<Rabbit, Hole>> jump(Direction direction, PMap<Coordinate, BoardItem> slice) throws InvalidMoveException {
         if (!containingItem.isPresent()){
             throw new InvalidMoveException("The hole does not contain a rabbit.");
