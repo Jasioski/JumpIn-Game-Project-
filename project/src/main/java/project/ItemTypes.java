6e038f83f633987e3fd7c19c32fa44a00610cff6
Rafi Khan <rafi@rafikhan.io>
2019-12-02 20:32:25 -0500

    added basic gui for the level builder
    just missing the fox stuff

---
+++ project/src/main/java/project/ItemTypes.java
@@ -0,0 +1,31 @@
+package project;
+
+public enum ItemTypes {
+    EmptyItem("Empty Item"),
+    Rabbit("Rabbit"),
+    Mushroom("Mushroom"),
+    EmptyHole("Empty Hole"),
+    EmptyElevated("Empty Elevated position"),
+    HoleRabbit("Hole with a Rabbit"),
+    ElevatedRabbit("Elevated with a rabbit"),
+    HoleMushroom("Hole with a mushroom"),
+    ElevatedMushroom("Elevated with a mushroom");
+
+    private String type;
+
+    ItemTypes(String s) {
+        this.type = s;
+    }
+
+    public String getType() { return this.type; };
+
+    public static ItemTypes fromString (String text) {
+        for (ItemTypes t : ItemTypes.values()) {
+            if (t.type.equalsIgnoreCase(text)) {
+                return t;
+            }
+        }
+
+        throw new IllegalArgumentException("unknown item type");
+    }
+}
